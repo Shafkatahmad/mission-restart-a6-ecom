79cf38a78081149d022214d0e56709a3b4c49c43
Shafkatahmad <shafkataahmad@gmail.com>
2026-02-17 00:44:04 +0600

    banner layout and why choose us sections

--- index.js
+++ index.js
@@ -28,17 +28,20 @@ const displayAllProducts = (products) => {
 
   for(let product of products) {
     const div = document.createElement("div");
+    div.classList.add('border-2', 'border-gray-200', 'rounded-xl');
     div.innerHTML = `
-    <img class="bg-gray-200 h-2/3 w-full p-4 object-contain mb-2" src=${product.image} alt="">
-    <div class="flex justify-between text-gray-500 mb-3">
-        <p class="bg-purple-200 text-purple-700 text-sm px-2 rounded-xl">${product.category}</p>
-        <p>${product.rating.rate} <span>(${product.rating.count})</span></p>
-    </div>
-        <h3 class="overflow-x-hidden font-semibold">${product.title}</h3>
+    <img class="bg-gray-200 h-4/6 w-full p-4 object-contain rounded-t" src=${product.image} alt="">
+    <div class="p-3">
+        <div class="flex justify-between text-gray-500 mb-3">
+            <p class="bg-purple-200 text-purple-700 text-sm px-2 rounded-xl">${product.category}</p>
+            <p>${product.rating.rate} <span>(${product.rating.count})</span></p>
+        </div>
+        <h3 class="truncate font-semibold">${product.title}</h3>
         <h3 class="font-bold">${product.price}</h3>
-    <div>
-        <button class="px-3 py-2 bg-red-300 text-sm rounded-xl">Details</button>
-        <button class="px-3 py-2 bg-blue-600 text-sm text-white rounded-xl">Add</button>
+        <div class="mt-3">
+            <button class="px-4 py-2 bg-white border-2 border-gray-200 text-gray-500 font-semibold text-sm rounded-xl"><i class="fa-regular fa-eye mr-2"></i>Details</button>
+            <button class="px-4 py-2 bg-blue-600 text-sm text-white rounded-xl"><i class="fa-solid fa-cart-arrow-down mr-2"></i>Add</button>
+        </div>
     </div>
     `
     productsContainer.appendChild(div);
